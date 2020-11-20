49c244e4db67268af943152a126986c8ec26a604
wangjinfeng <jinfeng2.wang@symbio.com>
2020-11-20 16:28:18 +0800

    ‘m

--- src/jfYu.Core/jfYu.Core.Data/DbContextServiceExtensions.cs
+++ src/jfYu.Core/jfYu.Core.Data/DbContextServiceExtensions.cs
@@ -1,8 +1,10 @@
 ﻿using Autofac;
+using Autofac.Core;
 using jfYu.Core.Common.Configurations;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Reflection;
 
 namespace jfYu.Core.Data
 {
@@ -25,60 +27,74 @@ namespace jfYu.Core.Data
             {
                 throw new Exception($"连接字符串配置错误:{ex.Message} - {ex.StackTrace}");
             }
-            //注册MasterDBContext
-            var masterOptBuilder = new DbContextOptionsBuilder<T>();
-            if (Config.DatabaseType.Equals(DatabaseType.SqlServer))
-                masterOptBuilder.UseLazyLoadingProxies().UseSqlServer(Config.MasterConnectionString);
-            else if (Config.DatabaseType.Equals(DatabaseType.Mysql))
-                masterOptBuilder.UseLazyLoadingProxies().UseMySQL(Config.MasterConnectionString);
-            services.RegisterType<T>().AsSelf().InstancePerLifetimeScope().WithParameter("options", masterOptBuilder.Options).Named<T>("MasterContext");
 
-            //注册SalveDBContext
-            int slaveCount = Config.SlaveConnectionStrings.Count;
-            if (slaveCount == 1)
-                services.RegisterType<T>().AsSelf().InstancePerLifetimeScope().WithParameter("options", masterOptBuilder.Options).Named<T>("SalveContext");
-            else
+            try
             {
-                Random r = new Random();
-                int i = r.Next(0, slaveCount);//随机一个从数据库
-                var SlaveOptBuilder = new DbContextOptionsBuilder<T>();
-                string SlaveConnectionString = Config.SlaveConnectionStrings[i].ConnectionString;
-                if (Config.DatabaseType.Equals(DatabaseType.SqlServer))
-                    SlaveOptBuilder.UseLazyLoadingProxies().UseSqlServer(SlaveConnectionString);
-                else if (Config.DatabaseType.Equals(DatabaseType.Mysql))
-                    SlaveOptBuilder.UseLazyLoadingProxies().UseMySQL(SlaveConnectionString);
-                services.RegisterType<T>().AsSelf().InstancePerLifetimeScope().WithParameter("options", SlaveOptBuilder.Options).Named<T>("SalveContext");
-
-                //for (int i = 0; i < slaveCount; i++)
-                //{
-                //    var SlaveOptBuilder = new DbContextOptionsBuilder<T>();
-                //    string SlaveConnectionString = Config.SlaveConnectionStrings[i].ConnectionString;
-                //    if (Config.DatabaseType.Equals(DatabaseType.SqlServer))
-                //        SlaveOptBuilder.UseLazyLoadingProxies().UseSqlServer(SlaveConnectionString);
-                //    else if (Config.DatabaseType.Equals(DatabaseType.Mysql))
-                //        SlaveOptBuilder.UseLazyLoadingProxies().UseMySQL(SlaveConnectionString);
-                //    services.RegisterType<T>().AsSelf().InstancePerLifetimeScope().WithParameter("options", SlaveOptBuilder.Options).Named<DbContext>($"SlaveContext{i + 1}");
-                //}
+                RegisterService<T>(services, Config);
             }
+            catch (Exception ex)
+            {
+                throw new Exception($"配置错误无法实例化主从连接:{ex.Message} - {ex.StackTrace}");
+            }
+           
 
-            //注册读写分离模块
-            //services.RegisterType<DbContextService<T>>()
-            //    .WithParameter((p, c) => p.Name == "MasterContext", (p, c) => c.ResolveNamed<T>("MasterContext"))
-            //    .WithParameter((p, c) => p.Name == "SalveContext", (p, c) => c.ResolveNamed<T>("SalveContext"))
-            //    .AsImplementedInterfaces().InstancePerLifetimeScope();
 
-            services.Register((c,p)=>new DbContextService<T>(c.ResolveNamed<T>("MasterContext"), c.ResolveNamed<T>("SalveContext")))
-                //.WithParameter((p, c) => p.Name == "MasterContext", (p, c) => c.ResolveNamed<T>("MasterContext"))
-                //.WithParameter((p, c) => p.Name == "SalveContext", (p, c) => c.ResolveNamed<T>("SalveContext"))
-                .AsImplementedInterfaces().InstancePerLifetimeScope();
         }
 
-        ///// <summary>
-        ///// IOC注册
-        ///// </summary>   
-        //public static void AddDbContextService<T>(this ContainerBuilder services, DatabaseConfiguration databaseConfiguration) where T : DbContext
-        //{
-        //    services.Register(q => new DbContextService<T>(databaseConfiguration)).AsImplementedInterfaces().InstancePerLifetimeScope();
-        //}
+        /// <summary>
+        /// IOC注册
+        /// </summary>   
+        public static void AddDbContextService<T>(this ContainerBuilder services, DatabaseConfiguration databaseConfiguration) where T : DbContext
+        {            
+            try
+            {
+                RegisterService<T>(services, databaseConfiguration);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"配置错误无法实例化主从连接:{ex.Message} - {ex.StackTrace}");
+            }
+        }
+
+        private static void RegisterService<T>(ContainerBuilder services,DatabaseConfiguration config) where T : DbContext
+        {
+            //注册MasterDBContext
+            var masterOptBuilder = new DbContextOptionsBuilder<T>();
+            if (config.DatabaseType.Equals(DatabaseType.SqlServer))
+                masterOptBuilder.UseLazyLoadingProxies().UseSqlServer(config.MasterConnectionString);
+            else if (config.DatabaseType.Equals(DatabaseType.Mysql))
+                masterOptBuilder.UseLazyLoadingProxies().UseMySQL(config.MasterConnectionString);
+            services.RegisterType<T>().AsSelf().InstancePerLifetimeScope().WithParameter("options", masterOptBuilder.Options).Named<T>("MasterContext");
+
+            //注册SalveDBContext  
+            int slaveCount = config.SlaveConnectionStrings.Count;
+            if (slaveCount > 0)
+            {
+                for (int i = 0; i < slaveCount; i++)
+                {
+                    var SlaveOptBuilder = new DbContextOptionsBuilder<T>();
+                    string SlaveConnectionString = config.SlaveConnectionStrings[i].ConnectionString;
+                    if (config.DatabaseType.Equals(DatabaseType.SqlServer))
+                        SlaveOptBuilder.UseLazyLoadingProxies().UseSqlServer(SlaveConnectionString);
+                    else if (config.DatabaseType.Equals(DatabaseType.Mysql))
+                        SlaveOptBuilder.UseLazyLoadingProxies().UseMySQL(SlaveConnectionString);
+                    services.RegisterType<T>().AsSelf().InstancePerLifetimeScope().WithParameter("options", SlaveOptBuilder.Options).Named<T>($"SlaveContext{i + 1}");
+                }
+            }
+
+            Random r = new Random();
+            var mParaVal = new Func<ParameterInfo, IComponentContext, object>((p, c) => c.ResolveNamed<T>("MasterContext"));
+            var salveContexts = new List<Func<ParameterInfo, IComponentContext, object>>();
+            for (int j = 0; j < slaveCount; j++)
+            {
+                string slaveContextName = $"SlaveContext{j + 1}";
+                salveContexts.Add(new Func<ParameterInfo, IComponentContext, object>((p, c) => c.ResolveNamed<T>(slaveContextName)));
+            }
+
+            services.RegisterType<DbContextService<T>>()
+                .WithParameter((p, c) => p.Name == "MasterContext", mParaVal)
+                .WithParameter((p, c) => p.Name == "SalveContext", slaveCount <= 0 ? mParaVal : salveContexts[r.Next(0, slaveCount)])
+                .AsImplementedInterfaces().InstancePerLifetimeScope();
+        }
     }
 }
